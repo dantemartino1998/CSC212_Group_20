de55cb6390330f63460dfad7acd003c1d73a908d
aidansussillo <90479473+aidansussillo@users.noreply.github.com>
2023-12-02 11:46:46 -0500

    Update main.cpp

--- programFiles/main.cpp
+++ programFiles/main.cpp
@@ -4,5 +4,24 @@
 #include <sstream>
 
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl; //if no input file is provided
+        return 1;
+    }
+
+    SplayTree splayTree;
+    std::ifstream inputFile(argv[1]);
+
+    if (inputFile.is_open()) { //reads input file and adds words to the Splay Tree
+        std::string word;
+        while (inputFile >> word) {
+            splayTree.insert(word);
+        }
+
+        inputFile.close(); //closes input file
+    } else {
+        std::cerr << "Error: Unable to open input file." << std::endl; //error handling
+        return 1;
+    }
     
 }
