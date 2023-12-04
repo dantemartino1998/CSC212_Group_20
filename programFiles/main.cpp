977395b1556daabeee10e28eb8764f966001e000
aidansussillo <90479473+aidansussillo@users.noreply.github.com>
2023-12-04 16:39:52 -0500

    Update main.cpp
    implemented menu for user to pick operation

--- programFiles/main.cpp
+++ programFiles/main.cpp
@@ -25,7 +25,53 @@ int main(int argc, char* argv[]) { //handles command line argument. requires inp
     }
 
     //menu for user to pick operation goes here. this loop continues until user inputs option 3.
+    while (true) {
+        std::cout << "Choose an operation:" << std::endl;
+        std::cout << "1. Search for an element" << std::endl;
+        std::cout << "2. Insert an element" << std::endl;
+        std::cout << "3. Quit" << std::endl;
 
+        int choice;
+        std::cin >> choice; //takes user input for which operation to do
 
+        if (choice == 1) {
+            //search operation
+            std::string target;
+            std::cout << "Enter a word to search (or 'q' to go back to the menu): ";
+            std::cin >> target;
+
+            if (target == "q") { //goes back to menu if user inputs q
+                continue;
+            }
+
+            auto result = splayTree.search(target); //perform search in Splay Tree for entered word
+            if (result.first) {//if word is found, display the word and its current count.
+                std::cout << "Found \"" << target << "\" in the tree. Count: " << result.second << std::endl;
+            } else {//if word isn't found, prints message.
+                std::cout << "\"" << target << "\" not found in the tree." << std::endl;
+            }
+        } else if (choice == 2) {
+            //insert operation
+            std::string newWord;
+            std::cout << "Enter a word to insert (or 'q' to go back to the menu): ";
+            std::cin >> newWord;
+
+            if (newWord == "q") {//enter q to quit
+                continue;
+            }
+
+            splayTree.insert(newWord);//inserts word into Splay Tree
+            std::cout << "\"" << newWord << "\" inserted into the tree." << std::endl;
+        } else if (choice == 3) {
+            //quit the program
+            break;
+        } else {//error handling for invalid choice.
+            std::cout << "Invalid choice. Please enter a valid option." << std::endl;
+        }
+
+        //generate DOT file for visualization after each operation
+        splayTree.generateDotFile("splay_tree.dot");
+    }
     
+    return 0;
 }
